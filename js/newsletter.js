89496e55b1583493e68fa0639c12ab4280f57166
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-07-18 16:53:27 -0300

    Vídeo 4.2: alterando a regex

--- js/newsletter.js
+++ js/newsletter.js
@@ -4,7 +4,7 @@ document.querySelector('.newsletter').addEventListener('submit', function (event
   const email = document.querySelector('.newsletter__input').value;
 
   // Regex para validar o e-mail
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]{2,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,8}$/;
 
   if (emailRegex.test(email)) {
     alert("E-mail válido! Inscrição realizada.");
