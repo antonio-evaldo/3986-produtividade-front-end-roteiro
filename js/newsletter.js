f528f4533f4beeffe49c07747f5d516afdf805c0
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-07-19 15:28:57 -0300

    Vídeo 5.2: implementando o dialog

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,14 +1,29 @@
-document.querySelector('.newsletter').addEventListener('submit', function (event) {
+const inputEmail = document.querySelector('.newsletter__input');
+const formNewsletter = document.querySelector('.newsletter');
+const dialog = document.querySelector('.modal-inscricao');
+const botaoFecharModal = document.querySelector('.modal-inscricao__fechar-modal');
+
+formNewsletter.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const email = document.querySelector('.newsletter__input').value;
-
-  // Regex para validar o e-mail
+  const email = inputEmail.value;
   const emailRegex = /^[a-zA-Z0-9._%+-]{2,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,8}$/;
 
   if (emailRegex.test(email)) {
-    alert("E-mail válido! Inscrição realizada.");
+    dialog.showModal();
   } else {
     alert('E-mail inválido!');
+  }
+});
+
+// Fecha o diálogo quando o botão "Fechar" for clicado
+botaoFecharModal.addEventListener("click", function () {
+  dialog.close();
+});
+
+// Fecha o diálogo quando o usuário clicar fora do modal
+dialog.addEventListener('click', function (event) {
+  if (event.target === dialog) {
+    dialog.close();
   }
 });
